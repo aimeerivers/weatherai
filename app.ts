1bcc467450fa347f4e830c32ba53af07b29f02df
aimee rivers <aimee@aimeerivers.com>
2023-06-25 12:03:07 +0200

    style(eslint): apply automatic lint fixes

--- app.ts
+++ app.ts
@@ -1,41 +1,41 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import axios from 'axios';
-import { Configuration, OpenAIApi } from 'openai';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from 'express'
+import bodyParser from 'body-parser'
+import axios from 'axios'
+import { Configuration, OpenAIApi } from 'openai'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static('public'));
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static('public'))
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+  apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(configuration)
 
 app.get('/data', async (req, res) => {
   try {
-    const currentTime = req.query.currentTime || new Date().toLocaleString();
-    let ipaddress = req.ip;
+    const currentTime = req.query.currentTime || new Date().toLocaleString()
+    let ipaddress = req.ip
     // Check if the request is coming from localhost
     if (ipaddress === '::1' || ipaddress === '::ffff:127.0.0.1') {
-      ipaddress = process.env.FALLBACK_IP_ADDRESS!;
+      ipaddress = process.env.FALLBACK_IP_ADDRESS!
     }
 
     // Use IP-based geolocation service to determine the user's location
-    const geoResponse = await axios.get(`https://ipapi.co/${ipaddress}/json/`);
-    const { city, region, country_name } = geoResponse.data;
+    const geoResponse = await axios.get(`https://ipapi.co/${ipaddress}/json/`)
+    const { city, region, country_name } = geoResponse.data
 
     // Fetch weather data for the location
     const weatherResponse = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${city},${country_name}&units=metric&appid=${process.env.WEATHER_API_KEY}`
-    );
-    const weather = weatherResponse.data.weather[0].description;
-    const temperature = weatherResponse.data.main.feels_like.toFixed(0);
+    )
+    const weather = weatherResponse.data.weather[0].description
+    const temperature = weatherResponse.data.main.feels_like.toFixed(0)
 
     // Use OpenAI's GPT-4 to generate an image prompt based on the location and weather
     const gptResponse = await openai.createCompletion({
@@ -66,17 +66,17 @@ app.get('/data', async (req, res) => {
         temperature: ${temperature} degrees celsius
         prompt:
         `,
-      max_tokens: 100,
-    });
-    const imagePrompt = gptResponse.data.choices[0].text!.trim();
+      max_tokens: 100
+    })
+    const imagePrompt = gptResponse.data.choices[0].text!.trim()
 
     // Use OpenAI's DALL-E API to generate an image
     const imageResponse = await openai.createImage({
       prompt: imagePrompt,
       n: 1,
-      size: '1024x1024',
-    });
-    const image = imageResponse.data.data[0].url;
+      size: '1024x1024'
+    })
+    const image = imageResponse.data.data[0].url
 
     // Send data to the frontend
     res.json({
@@ -85,13 +85,13 @@ app.get('/data', async (req, res) => {
       weather,
       temperature,
       imagePrompt,
-      image,
-    });
+      image
+    })
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch data' });
+    console.error(error)
+    res.status(500).json({ error: 'Failed to fetch data' })
   }
-});
+})
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const port = process.env.PORT || 3000
+app.listen(port, () => { console.log(`Server is running on port ${port}`) })
